a111fa818b14b78cf7968d56c097bf9df534ae7e
keeser <1326658417@qq.com>
2023-10-28 02:38:27 +0800

    修复内存泄漏

--- include/Utils.h
+++ include/Utils.h
@@ -8,22 +8,24 @@ const char *const RED = "\033[31m";
 const char *const GREEN = "\033[32m";
 const char *const BLACK = "\033[0m";
 
-static void printError(const char *str)
+void printError(const char *str)
 {
     char *print_str = (char *)malloc(strlen(RED) + strlen(str) + strlen(BLACK));
     strcpy(print_str, RED);
     strcat(print_str, str);
     strcat(print_str, BLACK);
     printf("%s\n", print_str);
+    free(print_str);
 }
 
-static void printSuccess(const char *str)
+void printSuccess(const char *str)
 {
     char *print_str = (char *)malloc(strlen(GREEN) + strlen(str) + strlen(BLACK));
     strcpy(print_str, GREEN);
     strcat(print_str, str);
     strcat(print_str, BLACK);
     printf("%s\n", print_str);
+    free(print_str);
 }
 
 #endif
